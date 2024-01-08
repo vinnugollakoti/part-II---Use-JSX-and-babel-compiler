b75182a8d1afdf54674987141787d681f575e2dc
vinay reddy gollakoti <145451307+vinnugollakoti@users.noreply.github.com>
2024-01-08 09:51:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+class ReactContainer extends React.Component{
+    // JSX tags
+    // JSX there should be one parent element
+      render(){
+          return (
+          <div>Hello! Welcome to Kalvium   
+             <div>This is babel</div>
+         </div> )
+      };  
+  }
+
+const container = document.getElementById('react-container');
+// Direclty pass the component to render
+ReactDOM.render(<ReactContainer/>,container);    
